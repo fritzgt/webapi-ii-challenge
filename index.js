5b73a00e2256852ed03da4e51ec14fb9495da1fc
fritzgt <tuanis@live.com>
2019-07-26 09:36:49 -0400

    created the posts route

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
+//importing the posts CRUD module
+const postsRoutes = require('./posts/postsRoutes');
+
 const server = express();
 
 const port = 5000;
 
+server.use('/posts', postsRoutes);
+
+// default route
 server.use('/', (req, res) => {
   res.status(200).send('Hello from express');
 });
